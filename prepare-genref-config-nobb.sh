a3b70c0ba5e0d36d7d81fec6926be9f496cab36a
Enade P. Istyastono <enade.istyastono@gmail.com>
2020-08-12 16:07:32 +0700

    Create prepare-genref-config-nobb.sh

---
+++ prepare-genref-config-nobb.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Before running this script, the pdb reference should be pre-prepared using module splitpdb from SPORES
+# These variables below should be adapted
+PLANTS="/home/ubuntu/programs/PLANTS64/PLANTS1.2"
+ligand="ligand_HUX803_0"
+radius="5"
+# From this point, the script will run smoothly if the dependencies are complete #
+cp $ligand.mol2 ligand_ref.mol2
+$PLANTS --mode bind ligand_ref.mol2 $radius protein.mol2
+echo "proteins protein.mol2" > genref-config-nobb.txt
+echo "ligands ligand_ref.mol2" >> genref-config-nobb.txt
+echo "outfile ref-results-nobb.txt" >> genref-config-nobb.txt
+echo "output_mode full_nobb" >> genref-config-nobb.txt
+echo "residue_number `grep CA PLANTSactiveSiteResidues.mol2 | awk '{print $7}' | paste -s -d" "`" >> genref-config-nobb.txt
+echo "residue_name `grep CA PLANTSactiveSiteResidues.mol2 | awk '{print $8}' | paste -s -d" "`" >> genref-config-nobb.txt
