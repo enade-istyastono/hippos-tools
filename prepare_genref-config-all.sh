b7f0c763cfd29bcb29916823c4988676ef5cb427
Enade P. Istyastono <enade.istyastono@gmail.com>
2020-08-28 12:46:51 +0700

    Update and rename prepare_genref-config-nobb.sh to prepare_genref-config-all.sh
    HIPPOS has been updated to 0.1.2. The bugs for simplified output mode and the identifications of CYS disulfide bridge have been fixed. Therefore it is not necessary to create reference for only one output mode.

--- prepare_genref-config-all.sh
+++ prepare_genref-config-all.sh
@@ -9,7 +9,7 @@ cp $ligand.mol2 ligand_ref.mol2
 $PLANTS --mode bind ligand_ref.mol2 $radius protein.mol2
 echo "proteins protein.mol2" > genref-config-nobb.txt
 echo "ligands ligand_ref.mol2" >> genref-config-nobb.txt
-echo "outfile ref-results-nobb.txt" >> genref-config-nobb.txt
-echo "output_mode full_nobb" >> genref-config-nobb.txt
+echo "outfile ref-results-all.txt" >> genref-config-nobb.txt
+echo "output_mode full full_nobb simplified" >> genref-config-nobb.txt
 echo "residue_number `grep CA PLANTSactiveSiteResidues.mol2 | awk '{print $7}' | paste -s -d" "`" >> genref-config-nobb.txt
 echo "residue_name `grep CA PLANTSactiveSiteResidues.mol2 | awk '{print $8}' | paste -s -d" "`" >> genref-config-nobb.txt
